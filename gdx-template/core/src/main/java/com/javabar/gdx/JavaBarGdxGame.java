31518a911323531ef95d385e3d6bccd7ce93f5a3
LuxZen-Tao <22331820@bucks.ac.uk>
2026-02-12 00:45:14 +0000

    first commitagain

---
+++ gdx-template/core/src/main/java/com/javabar/gdx/JavaBarGdxGame.java
@@ -0,0 +1,21 @@
+package com.javabar.gdx;
+
+import com.badlogic.gdx.Game;
+
+public class JavaBarGdxGame extends Game {
+    private final SimBridge simBridge = new SimBridge();
+    private final AudioSettings audioSettings = new AudioSettings();
+
+    @Override
+    public void create() {
+        setScreen(new BootScreen(this));
+    }
+
+    public SimBridge simBridge() {
+        return simBridge;
+    }
+
+    public AudioSettings audioSettings() {
+        return audioSettings;
+    }
+}
